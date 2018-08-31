375b7f9dfc858de6702134bb261e83dc82fa4f9c
waqar hameed <waqarooohameed@gmail.com>
2018-08-31 22:07:51 +0500

    PrimitiveDataTypes.java file

---
+++ PrimitiveDataTypes.java
@@ -0,0 +1,25 @@
+
+public class PrimitiveDataTypes {
+
+	public static void main(String[] args) {
+  
+		byte zee = 127;
+		short zea = 32767;
+		int i=1234567890;
+		float f=0.0f;
+		double d=1234567890;
+		long l=123456789123456789;
+		boolean B=false;
+		char c='\uffff';
+		System.out.println("byte range =" + zee);
+		System.out.println("short range=" + zea);
+		System.out.println("int range=" + i);
+		System.out.println("float range =" + f);
+		System.out.println("double range =" + d);
+		System.out.println("char range =" + c);
+		System.out.println("Boolean range=" + B);
+		System.out.println("long range ="  + l);
+
+
+	}
+}
